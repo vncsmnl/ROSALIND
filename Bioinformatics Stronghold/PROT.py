c550c7b97cd447cbeb86489ba9b6886902b57c11
Vinícius Manoel  <59481808+vncsmnl@users.noreply.github.com>
2026-04-02 19:31:52 -0300

    Add solution for Translating RNA into Protein problem in bioinformatics

---
+++ Bioinformatics Stronghold/PROT.py
@@ -0,0 +1,87 @@
+## Rosalind: Bioinformatics Stronghold
+## Problem: Translating RNA into Protein
+## Vinícius Manoel
+
+codon_table = {
+    "UUU": "F",
+    "CUU": "L",
+    "AUU": "I",
+    "GUU": "V",
+    "UUC": "F",
+    "CUC": "L",
+    "AUC": "I",
+    "GUC": "V",
+    "UUA": "L",
+    "CUA": "L",
+    "AUA": "I",
+    "GUA": "V",
+    "UUG": "L",
+    "CUG": "L",
+    "AUG": "M",
+    "GUG": "V",
+    "UCU": "S",
+    "CCU": "P",
+    "ACU": "T",
+    "GCU": "A",
+    "UCC": "S",
+    "CCC": "P",
+    "ACC": "T",
+    "GCC": "A",
+    "UCA": "S",
+    "CCA": "P",
+    "ACA": "T",
+    "GCA": "A",
+    "UCG": "S",
+    "CCG": "P",
+    "ACG": "T",
+    "GCG": "A",
+    "UAU": "Y",
+    "CAU": "H",
+    "AAU": "N",
+    "GAU": "D",
+    "UAC": "Y",
+    "CAC": "H",
+    "AAC": "N",
+    "GAC": "D",
+    "UAA": "Stop",
+    "CAA": "Q",
+    "AAA": "K",
+    "GAA": "E",
+    "UAG": "Stop",
+    "CAG": "Q",
+    "AAG": "K",
+    "GAG": "E",
+    "UGU": "C",
+    "CGU": "R",
+    "AGU": "S",
+    "GGU": "G",
+    "UGC": "C",
+    "CGC": "R",
+    "AGC": "S",
+    "GGC": "G",
+    "UGA": "Stop",
+    "CGA": "R",
+    "AGA": "R",
+    "GGA": "G",
+    "UGG": "W",
+    "CGG": "R",
+    "AGG": "R",
+    "GGG": "G",
+}
+
+rna = input("Enter the RNA sequence: ").strip()
+protein = ""
+
+for i in range(0, len(rna), 3):
+    codon = rna[i : i + 3]
+    if len(codon) < 3:
+        break
+    if codon_table[codon] == "Stop":
+        break
+    protein += codon_table[codon]
+
+result = protein
+
+print("\n" + "=" * 30)
+print(f"The resulting amino acid sequence is: {result}")
+print("=" * 30)
