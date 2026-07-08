d67e6be2bdf24cf9eb5f27626c544751661fd055
Vinícius Manoel  <59481808+vncsmnl@users.noreply.github.com>
2026-07-07 21:44:03 -0300

    Refactor INI6.py to streamline word counting and remove unnecessary function

--- Python Village/INI6.py
+++ Python Village/INI6.py
@@ -3,23 +3,11 @@
 ## Vinícius Manoel
 
 
-def count_word(s):
-    words = s.split(" ")
-
+with open("rosalind_ini6.txt") as infile:
     word_counts = {}
 
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
+    for word in infile.read().split():
+        word_counts[word] = word_counts.get(word, 0) + 1
 
-    # Print
-    for key, value in word_counts.items():
-        print(f"{key} {value}")
-
-
-# Sample Dataset
-with open("rosalind_ini6.txt", "r") as infile:
-    dataset = infile.read().strip()
-count_word(dataset)
+    for word, count in word_counts.items():
+        print(word, count)
